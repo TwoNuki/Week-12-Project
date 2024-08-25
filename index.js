3a723e67655fd2664c955772153b5958b0b7c656
TwoNuki <shadowchaos1@gmail.com>
2024-08-24 18:30:27 -0700

    final commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Create a CRD application (CRUD without update) using json-server or another API
+// Use fetch and async/await to interact with the API
+// Use a form to create/post new entities
+// Build a way for users to delete entities
+// Include a way to get entities from the API and display them
+// You do NOT need update, but you can add it if you'd like
+// Use Bootstrap and/or CSS to style your project
+
 let url = 'https://66c550d4134eb8f43493fc51.mockapi.io/'
 
 //variable to set the container element
